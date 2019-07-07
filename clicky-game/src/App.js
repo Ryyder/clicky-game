06eade9f8bfb2e96b30ec8d2671047022c0f2802
Ryyder <brucecho82@hotmail.com>
2019-07-06 21:18:06 -0700

    added comments for shuffle and handleclick methods

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -5,16 +5,21 @@ import Title from "./components/Title";
 import Nav from "./components/Navbar";
 import friends from "./friends.json";
 
+//function that shuffle our cards
 const Shuffle = (arr) => {
+  //loop through our array...
   for (let i = 0; i < arr.length; i++) {
+    //randomly select a card
     let j = Math.floor(Math.random() * (i + 1));
+    //swap the values
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
+  //return our array
   return arr;
 }
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
+  // Setting this.state.friends to the friends json array, highscore, currentscore, status, and clicked array
   state = {
     friends,
     highScore: 0,
@@ -52,10 +57,15 @@ class App extends Component {
   //if the id's are not equal, then call our increment method
   //else the game restarts
   handleClick = id => {
+    //check if our clicked array contains any of the id's have not been clicked...so if not in our clicked array
     if (this.state.clicked.indexOf(id) === -1) {
+      //increment our score
       this.handleIncrement();
+      //add the id to the clicked array in our state object
       this.setState({clicked: this.state.clicked.concat(id)});
-    } else {
+    } 
+    //if same id clicked twice, we reset the game board
+    else { 
       this.handleReset();
     }
   };
@@ -85,7 +95,7 @@ class App extends Component {
         <Nav 
         title = "Naruto Click Game"
         score = {this.state.currentScore}
-        topScore = {this.state.topScore}
+        topScore = {this.state.highScore}
         status = {this.state.status}
         />
         <Title>Click on a Character! If you don't click on any duplicates twice in a row, your score goes up 1 point. Try to see how high you can score.</Title>
