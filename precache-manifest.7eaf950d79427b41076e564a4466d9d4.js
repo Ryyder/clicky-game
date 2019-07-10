9afec4a08d659ffce563b9d12ad628cbcfd589db
Ryyder <brucecho82@hotmail.com>
2019-07-09 20:10:28 -0700

    Updates

---
+++ precache-manifest.7eaf950d79427b41076e564a4466d9d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd9ed75149cc06ef6e305bc2a205bf4",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "42246aff0bb1ed378315",
+    "url": "/clicky-game/static/css/main.26778014.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/clicky-game/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "42246aff0bb1ed378315",
+    "url": "/clicky-game/static/js/main.d8f1491a.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
